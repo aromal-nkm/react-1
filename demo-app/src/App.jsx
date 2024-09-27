7f1375e6f982aca4d1437be88045ba6e1e4a976f
aromal-nkm <aromalts2002@gmail.com>
2024-09-27 13:26:52 +0530

    5

--- demo-app/src/App.jsx
+++ demo-app/src/App.jsx
@@ -4,14 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './components/Home'
 import Add from './components/Add'
+import Nav from './components/Nav'
+import {Route, Routes} from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <> 
-    <Home></Home> <br />
-    <Add></Add>
+    <Nav></Nav>
+   <Routes>
+    <Route path='/' element= {<Home/>}></Route>
+    <Route path='/add' element={<Add/>}></Route>
+
+   </Routes>
     
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
--- demo-app/src/components/Add.jsx
+++ demo-app/src/components/Add.jsx
@@ -1,20 +1,13 @@
+import { Button, TextField } from '@mui/material'
 import React from 'react'
-// import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+
 const Add = () => {
-  return (
-    <Box
-    component="form"
-    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField id="outlined-basic" label="EID" variant="outlined" /><br />
-    <TextField id="filled-basic" label="Employeename" variant="filled" /><br />
-    <TextField id="standard-basic" label="Designation" variant="standard" /><br />
-    <TextField id="standard-basic" label="Location" variant="standard" />
-  </Box>
+  return ( <div>
+    <TextField id="standard-basic" label="Employee ID" variant="standard" /><br />
+    <TextField id="standard-basic" label="Employee Name" variant="standard" /><br />
+    <TextField id="standard-basic" label="Employee dept" variant="standard" /><br />
+    <TextField id="standard-basic" label="Employee location" variant="standard" /> <br /><br />
+    <Button variant="contained">submit</Button> <br /></div>
   )
 }
 
